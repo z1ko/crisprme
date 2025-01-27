7bfc553808995471c23c8e257935fd6b9f5d043e
Filippo Ziche <filippo.ziche@gmail.com>
2025-01-27 22:43:42 +0100

    feat: implement utilities macros for initialization

--- crisprme-cuda/src/lib.rs
+++ crisprme-cuda/src/lib.rs
@@ -12,35 +12,48 @@ use std::time::Instant;
 
 // CUDA errors
 pub type KResult<T> = Result<T, DriverError>;
+// Wrapper for the CudaDevice
+pub type Gpu = Arc<CudaDevice>;
 
 // Include generated CUDA bindings
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-// Include the compiled PTX code as string
-const CUDA_KERNEL_PTX_EXAMPLE: &str = include_str!(concat!(env!("OUT_DIR"), "/example.ptx"));
+/// Macro used to create the initialize function
+macro_rules! create_initialize_func {
+    ($($ptx_file:literal),*) => {
+        paste! {
 
-/// Macro to generate boilerplater for kernel function with basic launch configuration
-macro_rules! generate_basic_kernel_call {
-    ($kernel_name:ident, $o:ty, $($v:tt: $t:ty),+) => { paste! {
-        /// Function that will be used to invoke the kernel by the consumer with simpler parameters
-        pub fn [< $kernel_name _simple >] (gpu: &Arc<CudaDevice>, n: u32, $($v: $t),+) -> KResult<$o> {
-            let config = LaunchConfig::for_num_elems(n as u32);
-            $kernel_name(gpu, config, n, $($v)+)
+            // Include all generated PTX files from the build system
+            $(
+                const [< PTX_ $ptx_file:upper >]: &str = include_str!(concat!(env!("OUT_DIR"), "/", $ptx_file, ".ptx"));
+            )*
+
+            /// Initialize Cuda device and load all PTX
+            pub fn initialize() -> KResult<Arc<CudaDevice>> {
+                let gpu = CudaDevice::new(0)?;
+
+                // Load all PTX sources to the GPU
+                $({
+                    println!("Loading source for {}:\n{}", concat!($ptx_file, ".ptx"), [< PTX_ $ptx_file:upper >]);
+                    let ptx = Ptx::from_src([< PTX_ $ptx_file:upper >]);
+                    gpu.load_ptx(ptx, "crisprme", &[ $ptx_file ])?;
+                })*
+
+                Ok(gpu)
+            }
         }
-    } };
+    }
 }
+
+create_initialize_func!("example");
 
 /// All the kernels will be inside this module
 pub mod kernel {
     use super::*;
 
     /// Function with customizable launch configuration
-    pub fn example(
-        gpu: &Arc<CudaDevice>,
-        config: LaunchConfig,
-        n: u32,
-        input: Vec<f32>,
-    ) -> KResult<Vec<f32>> {
+    pub fn example(gpu: &Gpu, config: LaunchConfig, input: Vec<f32>) -> KResult<Vec<f32>> {
+        let n = input.len();
         let input = gpu.htod_copy(input)?; // Copy input to GPU memory
         let f = gpu.get_func("crisprme", "example").unwrap();
         unsafe {
@@ -48,19 +61,6 @@ pub mod kernel {
         }
         gpu.sync_reclaim(input) // Retrive output
     }
-
-    generate_basic_kernel_call!(example, Vec<f32>, input: Vec<f32>);
-}
-
-/// Initialize Cuda device and load all PTX
-pub fn initialize() -> KResult<Arc<CudaDevice>> {
-    let gpu = CudaDevice::new(0)?;
-
-    // Load the ptx of the kernel
-    let ptx = Ptx::from_src(CUDA_KERNEL_PTX_EXAMPLE);
-    gpu.load_ptx(ptx, "crisprme", &["example"])?;
-
-    Ok(gpu)
 }
 
 /// Support function to automatize launch configuration
@@ -83,7 +83,7 @@ pub mod autotune {
     pub fn benchmark<F>(
         gpu: &Arc<CudaDevice>,
         search: Search,
-        mut f: F,
+        f: F,
     ) -> KResult<(LaunchConfig, u128)>
     where
         F: Fn(&Arc<CudaDevice>, LaunchConfig),
--- crisprme/src/main.rs
+++ crisprme/src/main.rs
@@ -1,15 +1,16 @@
-use crisprme_cuda::{KResult, LaunchConfig};
+use crisprme_cuda::{autotune, kernel, KResult, LaunchConfig};
 
 /// Example usage of kernels
 fn main() -> KResult<()> {
     let gpu = crisprme_cuda::initialize()?;
 
     // Launch work on GPU and get result
-    let output = crisprme_cuda::kernel::example(&gpu, vec![0.0; 1000])?;
+    let config = LaunchConfig::for_num_elems(1000);
+    let output = kernel::example(&gpu, config, vec![0.0; 1000])?;
     println!("{:?}", output);
 
     // Let's find the best launch configuration
-    let s = crisprme_cuda::autotune::Search {
+    let s = autotune::Search {
         configs: vec![
             LaunchConfig::for_num_elems(1000),
             LaunchConfig::for_num_elems(100),
@@ -18,9 +19,9 @@ fn main() -> KResult<()> {
     };
 
     println!(
-        "best configuration: {:?}",
-        crisprme_cuda::autotune::benchmark(&gpu, s, |gpu, c| {
-            crisprme_cuda::kernel::example_with_config(gpu, c, vec![0.0; 1000]).unwrap();
+        "Best configuration: {:?}",
+        autotune::benchmark(&gpu, s, |gpu, c| {
+            kernel::example(gpu, c, vec![0.0; 1000]).unwrap();
         })?
     );
     Ok(())
